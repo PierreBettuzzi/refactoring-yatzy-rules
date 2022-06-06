72aa5e25d4c2f02530151ebbe92922e54de17dd7
Pierre Bettuzzi <pierre.bettuzzi@gmail.com>
2022-06-06 19:21:34 +0200

    refactor: create constants

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -1,8 +1,16 @@
+
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
+    private static final int FOUR = 4;
+	private static final int THREE = 3;
+	private static final int TWO = 2;
+	private static final int ONE = 1;
+	private static final int FIVE = 5;
+	private static final int SIX = 6;
+	private static final int ZERO = 0;
+	public static int chance(int d1, int d2, int d3, int d4, int d5)
     {
-        int total = 0;
+        int total = ZERO;
         total += d1;
         total += d2;
         total += d3;
@@ -13,66 +21,66 @@ public class Yatzy {
 
     public static int yatzy(int... dice)
     {
-        int[] counts = new int[6];
+        int[] counts = new int[SIX];
         for (int die : dice)
-            counts[die-1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
+            counts[die-ONE]++;
+        for (int i = ZERO; i != SIX; i++)
+            if (counts[i] == FIVE)
                 return 50;
-        return 0;
+        return ZERO;
     }
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
+        int sum = ZERO;
+        if (d1 == ONE) sum++;
+        if (d2 == ONE) sum++;
+        if (d3 == ONE) sum++;
+        if (d4 == ONE) sum++;
+        if (d5 == ONE) 
             sum++;
 
         return sum;
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
+        int sum = ZERO;
+        if (d1 == TWO) sum += TWO;
+        if (d2 == TWO) sum += TWO;
+        if (d3 == TWO) sum += TWO;
+        if (d4 == TWO) sum += TWO;
+        if (d5 == TWO) sum += TWO;
         return sum;
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
         int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
+        s = ZERO;
+        if (d1 == THREE) s += THREE;
+        if (d2 == THREE) s += THREE;
+        if (d3 == THREE) s += THREE;
+        if (d4 == THREE) s += THREE;
+        if (d5 == THREE) s += THREE;
         return s;
     }
 
     protected int[] dice;
     public Yatzy(int d1, int d2, int d3, int d4, int _5)
     {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
+        dice = new int[FIVE];
+        dice[ZERO] = d1;
+        dice[ONE] = d2;
+        dice[TWO] = d3;
+        dice[THREE] = d4;
+        dice[FOUR] = _5;
     }
 
     public int fours()
     {
         int sum;    
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
+        sum = ZERO;
+        for (int at = ZERO; at != FIVE; at++) {
+            if (dice[at] == FOUR) {
+                sum += FOUR;
             }
         }
         return sum;
@@ -80,123 +88,123 @@ public class Yatzy {
 
     public int fives()
     {
-        int s = 0;
+        int s = ZERO;
         int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
-                s = s + 5;
+        for (i = ZERO; i < dice.length; i++) 
+            if (dice[i] == FIVE)
+                s = s + FIVE;
         return s;
     }
 
     public int sixes()
     {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
+        int sum = ZERO;
+        for (int at = ZERO; at < dice.length; at++) 
+            if (dice[at] == SIX)
+                sum = sum + SIX;
         return sum;
     }
 
     public static int score_pair(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
+        int[] counts = new int[SIX];
+        counts[d1-ONE]++;
+        counts[d2-ONE]++;
+        counts[d3-ONE]++;
+        counts[d4-ONE]++;
+        counts[d5-ONE]++;
         int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
-        return 0;
+        for (at = ZERO; at != SIX; at++)
+            if (counts[SIX-at-ONE] >= TWO)
+                return (SIX-at)*TWO;
+        return ZERO;
     }
 
     public static int two_pair(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
+        int[] counts = new int[SIX];
+        counts[d1-ONE]++;
+        counts[d2-ONE]++;
+        counts[d3-ONE]++;
+        counts[d4-ONE]++;
+        counts[d5-ONE]++;
+        int n = ZERO;
+        int score = ZERO;
+        for (int i = ZERO; i < SIX; i += ONE)
+            if (counts[SIX-i-ONE] >= TWO) {
                 n++;
-                score += (6-i);
+                score += (SIX-i);
             }        
-        if (n == 2)
-            return score * 2;
+        if (n == TWO)
+            return score * TWO;
         else
-            return 0;
+            return ZERO;
     }
 
     public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
     {
         int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
+        tallies = new int[SIX];
+        tallies[_1-ONE]++;
+        tallies[_2-ONE]++;
+        tallies[d3-ONE]++;
+        tallies[d4-ONE]++;
+        tallies[d5-ONE]++;
+        for (int i = ZERO; i < SIX; i++)
+            if (tallies[i] >= FOUR)
+                return (i+ONE) * FOUR;
+        return ZERO;
     }
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
     {
         int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
+        t = new int[SIX];
+        t[d1-ONE]++;
+        t[d2-ONE]++;
+        t[d3-ONE]++;
+        t[d4-ONE]++;
+        t[d5-ONE]++;
+        for (int i = ZERO; i < SIX; i++)
+            if (t[i] >= THREE)
+                return (i+ONE) * THREE;
+        return ZERO;
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
     {
         int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
+        tallies = new int[SIX];
+        tallies[d1-ONE] += ONE;
+        tallies[d2-ONE] += ONE;
+        tallies[d3-ONE] += ONE;
+        tallies[d4-ONE] += ONE;
+        tallies[d5-ONE] += ONE;
+        if (tallies[ZERO] == ONE &&
+            tallies[ONE] == ONE &&
+            tallies[TWO] == ONE &&
+            tallies[THREE] == ONE &&
+            tallies[FOUR] == ONE)
             return 15;
-        return 0;
+        return ZERO;
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
     {
         int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
+        tallies = new int[SIX];
+        tallies[d1-ONE] += ONE;
+        tallies[d2-ONE] += ONE;
+        tallies[d3-ONE] += ONE;
+        tallies[d4-ONE] += ONE;
+        tallies[d5-ONE] += ONE;
+        if (tallies[ONE] == ONE &&
+            tallies[TWO] == ONE &&
+            tallies[THREE] == ONE &&
+            tallies[FOUR] == ONE
+            && tallies[FIVE] == ONE)
             return 20;
-        return 0;
+        return ZERO;
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
@@ -204,36 +212,36 @@ public class Yatzy {
         int[] tallies;
         boolean _2 = false;
         int i;
-        int _2_at = 0;
+        int _2_at = ZERO;
         boolean _3 = false;
-        int _3_at = 0;
+        int _3_at = ZERO;
 
 
 
 
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
+        tallies = new int[SIX];
+        tallies[d1-ONE] += ONE;
+        tallies[d2-ONE] += ONE;
+        tallies[d3-ONE] += ONE;
+        tallies[d4-ONE] += ONE;
+        tallies[d5-ONE] += ONE;
 
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
+        for (i = ZERO; i != SIX; i += ONE)
+            if (tallies[i] == TWO) {
                 _2 = true;
-                _2_at = i+1;
+                _2_at = i+ONE;
             }
 
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
+        for (i = ZERO; i != SIX; i += ONE)
+            if (tallies[i] == THREE) {
                 _3 = true;
-                _3_at = i+1;
+                _3_at = i+ONE;
             }
 
         if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
+            return _2_at * TWO + _3_at * THREE;
         else
-            return 0;
+            return ZERO;
     }
 }
 
