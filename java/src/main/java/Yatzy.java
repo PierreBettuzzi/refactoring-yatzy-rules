d95a75a607131bb41637316a7cbafc87b463beb9
Pierre Bettuzzi <pierre.bettuzzi@gmail.com>
2022-06-06 19:29:02 +0200

    refactor: function chance

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -10,13 +10,7 @@ public class Yatzy {
 	private static final int ZERO = 0;
 	public static int chance(int d1, int d2, int d3, int d4, int d5)
     {
-        int total = ZERO;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+        return d1 + d2 +d3 +d4 +d5;
     }
 
     public static int yatzy(int... dice)
